af0fc75d5e9bff1e71b9ad552f5ebf6583befe2c
Dimar Ilham Tamara <dimartamara1@gmail.com>
2025-05-15 23:06:03 +0700

    Add message from Dimar

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Dimar\n");
+    printf("Tugas dan case study di praktikum progdas ini sangat membantu dalam mengasah skill programming saya, terutama di case study 1 di mana saya harus menyelesaikan masalah hanya dalam 50 menit. Soal-soal yang diberikan juga sangat menarik sehingga saya tidak bosan saat mengerjakannya. Untuk harapan saya kedepannya, saya bisa lebih siap menghadapi praktikum-praktikum pemrogramman selanjutnya seperti praktikum algoritma dan pemrogramman, object oriented programming, dan lainnya. Terima kasih banyak aslab-aslab digilab yang keren-keren karena sudah memberikan tugas dan case study yang sangat menantang :)");
 }
