cc610ff670486d4893a562cc4080488f456cbd73
niconett18 <nicholasedmund18@gmai.com>
2025-05-16 02:09:54 +0700

    Add message from <Mungkung>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Mungkung\n");
+    printf("tengkiu buat aslab aslab yang udah ajarin gw dari awal ngoding c, jujur dengan praktikum ini gw jadi kebantu banget buat ngikutin matkul progdas, dari pada dosen yang ajarin , pesen buat aslab semangat ngecek TP dan CS kita dan jangan pelit pelit nilai ya hehehe!\n");
 }
