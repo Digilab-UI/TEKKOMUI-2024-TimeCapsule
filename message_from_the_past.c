79658ba2b7c7350fd8f265fc8c4ba3ad32ec397c
Naputriz <naputrizwastaken@gmail.com>
2025-05-16 10:29:01 +0700

    Add message from Nabil Putra Nurfariz

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,8 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Naputriz. \n");
+    printf("Terima kasih kepada aslab untuk bimbingannya selama setahun ini. \n");
+    printf("Also I Love Firefly. \n");
 }
