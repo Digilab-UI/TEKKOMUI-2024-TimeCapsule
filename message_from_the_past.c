669fb2baa2ca537236e7ff945c077cd42ef36816
Reinathanajah <reinathanezhekiel1@gmail.com>
2025-05-17 00:19:00 +0700

    Nambahin pesan :) awoakwokoa,Reinathan Ezkhiel

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -91,5 +91,7 @@ int main() {
   
   printf("From:Reinathan \n");
   printf("Makasih Aslab Digilab udah bimbing saya di semester 2 ini , semoga sukses selalu");
+  printf("Jujur belajar banyak banget dari semester 2 ini , kadang ada pas praktikum ke trigger , kadang ada yang seneng banget , tapi semuanya keren dah out of the box. Aslabnya baik dan terbuka, keknya kalo ga ada abang - abang gabisa ngerjain awkoakwokaowkaowk. Pokoknya gacor dah semester 2 ini. Gataulah kalo semester 3 gimana :) semoga bisa ketemu aslab lagi di semester 3 semoga masih semangat di tekkom \n"); 
+  
     
 }
