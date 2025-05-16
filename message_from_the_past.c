02d38c3b6bdc0119cc9b7a24dd9e1260722dd383
Allmeerage <arkan.k06@gmail.com>
2025-05-16 17:07:43 +0700

    added my message

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,10 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    
+    printf("From: Kana\n");
+    printf("Terimakasih untuk semua aslab dan temen2 yg udh pernah ngebantuin gw selama DSD & progdas <3\n");
+    printf("Jangan lupakan kalkulator 1.5 jt itu </3\n");
+    printf("Gw jg blm balikin kabel micro usb ke digilab <///3 maaf...\n");
+    
 }
