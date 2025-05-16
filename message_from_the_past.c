051a5bbe854b4713545648ba4c117793ff8758df
Hadar15 <haidar.r2005@gmail.com>
2025-05-17 05:56:48 +0700

    Add Message from Haidar

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,10 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-}
+
+    printf("From Haidar\n");
+    printf("Terima kasih kepada semua aslab yang telah mengajar dengan baik dan juga berperilaku baik kepada para praktikannya.Semoga segala kebaikan yang diberikan kepada praktikan akan menjadi manfaat kembali untuk kedepannya");
+    printf("\nSetelah mengikuti rangkaian digilab dari awal hingga saat ini, saya merasa semakin paham terkait materinya, apalagi dibantu oleh para aslab yang baik");
+    printf("\nUntuk teman-teman tekkom semua, apapun yang kalian inginkan untuk dicapai, semoga semuanya bisa tercapai dengan baik");
+
+}   
