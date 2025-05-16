bf24c0e1d5c2dba319e174af426f96355487341d
mdaffrzk1 <mdaffar105@gmail.com>
2025-05-16 23:10:22 +0700

    Add message from Dapriz

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,10 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: MDR\n");
+    printf("Terima kasih kepada aslab-aslab yang sudah membimbing kami selama 10 modul di praktikum. Semoga semua ilmu yang diberikan dapat bermanfaat di kemudian hari. Sukses terus untuk para aslab, semoga rejeki lancar, semoga lulus cumlaude\n");
+    printf("W DigiLab\n");
+    printf("Jauhi narkoba\n");
+    printf("Jauhi judi online\n");
 }
