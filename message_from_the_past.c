d3886b731f8438d897442fad07e86788fcea79e3
ToriqFathoni <toriqfathoni01@gmail.com>
2025-05-16 08:02:20 +0700

    HMMM <toriq>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,16 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From : Siapa Be Dah yang Penting Orang ");
+    printf("Buat aslab-aslab ty ya udh bantu kita selama ini selama praktikum\n");
+    printf("Klo bisa pliss kasih video pembelajaran kaya dsd semester lalu T--T\n");
+    printf("Jujur itu membantu banget\n");
+    printf("oiya klo misalnya ini nanti bakal diliat sekian tahun lagi, w cuma mau bilang\n");
+    printf("Ty banget udh bantu selama ini dan misalnya nanti abang-abang aslab dh sukses dan dah kerja, jgn lupain kami yaa\n")
+    printf("sama jangan bosen bantu kami even itu di life after graduation\n");
+    printf("Buat temen-temen Tekkom juga makasih banyakk yawww\n");
+    printf("dah ah cape ngetik\n");
+    printf("hehe\n\n");
+
 }
