f3929b359304658e82a18f845a6e6c21bc29d6ba
AdeZaskiaAzzahra <adezaskiaazzahra@gmail.com>
2025-05-16 20:56:20 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,6 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("thank uu buat aslab yang udah sabar ngejelasin, walaupun kadang kita lebih banyak nanya salahnya di mana daripada nyoba benerin sendiri. babaii progdas BISMILLAH nilai aman \n")
 }
