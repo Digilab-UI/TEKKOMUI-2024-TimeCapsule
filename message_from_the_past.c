4d3049e3b335f5765c60b654908daf964ace75ec
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-16 22:51:14 +0700

    Add message from Zulfahmi

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,8 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Zulfahmi\n");
+    printf("Terima kasih bnyak untk para aslab yg setia mengajarii kami disetiap ksulitan, smoga kita bisa sukses bersamaa.\n");
+    
 }
