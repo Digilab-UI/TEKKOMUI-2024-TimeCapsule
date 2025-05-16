8dddd01acc9d397a9843b3ce5b1345721f599301
Rise The Rizzler <naziehan06@gmail.com>
2025-05-16 15:56:16 +0700

    update messaage

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     printf("\033[1;34m");
     printf("Written by Tekkom UI 2024\n");
     printf("\033[0m");
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Ziehan");
+    printf("this is me before coding with coding documentation")
 }
