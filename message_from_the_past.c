68206861b108f95cdd3d8594ea915cb23d1c8b4a
ksalsabilacaca <salsammm31@gmail.com>
2025-05-17 19:37:27 +0700

    Add message from Salsabila Maharani

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Salsabila Maharani Mumtaz\n");
+    printf("Terimakasih kepada semua aslab digilab yang sudah memberikan banyak ilmu dan \nbig thanks to Bang Tian karena asistensi OAK nya sangat membantu dan \ncara beliau menjelaskan mudah dipahami\n");
 }
