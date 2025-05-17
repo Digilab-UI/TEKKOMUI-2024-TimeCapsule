e1a0b1217adbca2ecc2c73365797e7bb209b4f97
Qais Ismail <qoisismail2@gmail.com>
2025-05-17 22:33:17 +0700

    Add message from Qais Ismail

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,6 +8,16 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
 
-    printf("From: NA1SS\n");
-    printf("We rise together, back to the moon and beyond\n");
+    printf("From: Qais, May 17 2025\n");
+    printf("Been a tough one huh? Congratulations! You've grown tough too.\n"
+           "As a random person on the internet said:\n"
+           "\"You may or may not have big victories, but I'm more amazed at the number of defeats you've come out alive\"\n"
+           "Keep in mind:\n"
+           "Per Aspera Ad Astra (A rough road leads to the stars).\n\n"
+           "Since you'd found your purpose in life, I wish you live to that purpose.\n"
+           "Now I'm passing the control over my destiny to you and I believe in you,\n"
+           "that you will guide this life on the right trajectory towards the moon and beyond.\n");
+
+    printf("\nAnd to all the readers\n"
+           "\"May your skies be blue, and your winds be low\" -Joe Barnard\n");
 }
