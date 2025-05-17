6f7da0ee1a22d65425bcbefe623db1978cc88d19
mmarzaki <abynrammar@gmail.com>
2025-05-17 17:55:02 +0700

    Added message form Abyan Ammar

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,9 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("whatever happens later in your life just follow your hearts okie :o \n");
+    printf("woy nonton liverpool langsung di anfield\n");
+    printf("ps. HIDUP ITU BERNAFAS\n\n");
+
 }
