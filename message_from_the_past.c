e0d31a5a389a9986433a622cf947a29f9e2bb7d6
zakialkhairi <muhammad.zaki46@ui.ac.id>
2025-05-17 13:45:59 +0700

    Add message from Rana

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,9 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("Untuk diri sendiri: semoga di masa depan makin suka programming atau paling nggak punya cukup uang buat ngejar impian\n");
+    printf("Jrcrd\n"); // CS1-7
+    printf("Xsrxpvm Qlxpvb\n"); // zgyzhs
+    printf("-Ufib\n");
 }
