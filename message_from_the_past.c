872d5fe8bd55492b8008e787b500f65f000a00ac
arido13 <aridhosectio@gmail.com>
2025-05-17 20:33:11 +0700

    Add message from Aridho

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    
+    printf("From rd, 17 of May 2025");
+	printf("Tetap semangat menghadapi apa yang ada di depan! \nRemember that you chose this path, you had all the time to decide what to do with your life back then \nJust follow YOUR way and don't forget to catch a breath :)");
 }
