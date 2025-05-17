1d00e0c2f0c455da42026461565aa50dea141b9a
RanaAqilaK <rana.aqilak@gmail.com>
2025-05-17 14:50:21 +0700

    Add message from zaki

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -5,6 +5,8 @@ int main() {
     printf("Written by Tekkom UI 2024\n");
     printf("\033[0m");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("buat aslab: buat bang MF, makasih bet bang asli rispek 999+ social credit indian respect button -> \n");
+    printf("buat temen seangkatan: sukses frens, info loker yak\n");
+    printf("buat diri sendiri: masih ngoding pake ai apa udh jadi panglima ngoding?\n");
+
 }
