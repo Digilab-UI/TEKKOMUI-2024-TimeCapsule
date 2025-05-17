b4efbeba8c05e478927687a981e08060558df80b
syifasip <syifaaulia18.a2@gmail.com>
2025-05-17 15:59:40 +0700

    Add message from syifa

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,13 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Anon\n");
+    printf("To: Aslab Progdas 2025\n");
+    printf("Thank you for being so incredibly patient guiding us, helping us,\n");
+    printf("and never letting us stay lost in this course.\n");
+    printf("For all the kindness you've shown, I genuinely hope life gives you\n");
+    printf("back even more good things in return.\n");
+    printf("Wishing you smooth thesis writing and a bright, fulfilling career ahead!\n");
+    
 }
