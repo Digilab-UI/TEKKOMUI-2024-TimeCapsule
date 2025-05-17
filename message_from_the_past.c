815d46e59ee01383477eb0031b405cdcc4e48040
syifanailaaa <syifanailamaulida@gmail.com>
2025-05-17 19:37:22 +0700

    Add message from <cipa>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,8 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("From: cipa naila\n");
+    printf("pertama-tama ak pengen banget ngucapin makasiii banyakkk kepada para aslab yang udh memberikan ilmunya dan juga tidak lupa temen temen aku yang slalu setia <3 lisa, yujin, didi, nadi, ajel, puti, mila ohh smoga kita wisuda bareng nantinya dan buat diri aku sendiri di masa depan selamat yahh kamu udh melewati segala hal di per tekkoman dan semoga pencapaianmu bermanfaat bagi sekitar dan masa depanmu!!! \n");
+
+    return 0;
 }
