0eca6145c2ce04cbe21fe66b7d43ae7c9cb32fc8
Zahab05 <Zahirshahab25@gmail.com>
2025-05-17 21:05:29 +0700

    Add message from Zahir

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,7 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From : zahir");
+    printf("Semoga saya bisa lulus tepat waktu, bisa dapat beasiswa, bisa jadi bph di exer, bisa jadi asdos, bisa ikut panit yg banyak, dan yang pasti bisa dapet kerjaan gaji 2 digit hehe.");
 }
