fba2e6fc40e4682e28102c0a972342608547ff60
AryaWA101 <aryawibawa1502@gmail.com>
2025-05-17 21:30:59 +0700

    Add message from Arya (apa gunanya anonim kalau ujungnya ada ini?)

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -4,7 +4,10 @@ int main() {
     printf("\033[1;34m");
     printf("Written by Tekkom UI 2024\n");
     printf("\033[0m");
-
+	
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    
+    printf("From : Orang Nomor 1\n");
+    printf("(>///<)HIDUP JOKOWIIII!!!");
 }
