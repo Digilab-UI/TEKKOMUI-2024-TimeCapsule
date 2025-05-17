e590b8014aa2bff93b06fa8f01815c6bf6e8630a
sutanren <rendysutan1@gmail.com>
2025-05-17 23:12:21 +0700

    Add message from sutan

---
+++ message_from_the_past1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("\033[1;34m");
+    printf("Sutan Rendy Rizaldi\n");
+    printf("\033[0m");
+
+    printf(" kerja kersa jangan ngeluh mulu \n");
+    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+}
