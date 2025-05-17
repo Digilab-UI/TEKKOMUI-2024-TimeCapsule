ba1996c3404f0195941ee6affb3d134776400480
Milannf <amyarfahmi@gmail.com>
2025-05-17 20:32:19 +0700

    Add message from Fahmi Milan Amyar

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,10 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("To Milan di tahun 2028, semoga lu masih berjuang, kalau belum sukses gpp, yang penting sabar dan belajar dari kesalahan");
+    printf("Semoga lu masih dekat sama Allah Swt, masih menuju jalan Allah Swt, semoga masih tetap sholat, masih tetap semangat");
+    printf("Hopefully what you have learnt from your Aslab, has brought you far, not just from Progdas, but from DSD and soon more");
+    printf("Semoga wishlist lu terpenuhi semua, dan lu tetap bersyukur apa adanaya");
+    printf("Dikit lagi gw akan ketemu lu, gw g berharap lu sukses, yang penting lu bahagia dan bersyukur sudah sejauh ini berjuangnya");
 }
