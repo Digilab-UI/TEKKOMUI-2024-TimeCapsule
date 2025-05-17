3841ff106dcbdad4edc0f29c7ad5abc87251260c
radyagardian <radya.pranoto@gmail.com>
2025-05-17 12:28:39 +0700

    Add message from Radya

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
-    printf("\033[0m");
+    
+    printf("Halo gw dari beberapa waktu di depan. Mulai dari mana yak, mungkin sekarang gue lagi nongkrong di gcer bareng yasmuh torsay dermuh wanmil sama king. Gw juga baru aja beli bluestates mad tiger 18oz, jujur agak gemeter WKWKWK. Gue lagi sibuk pars juga, ada project exer, sekum gladiator, pj dokum tc, sama ime kestari ofc. Jujur gw rada ga peduli akademik, cuma tetep ngerjain jalanin aja, gue juga lagi kultural. Jujur lagi capek bet, cuma ya itu aja sih, ga terlalu kenapa napa juga. \n");
+    printf("oke lanjut, semoga lu sekarang udah settle, aman aman aja, denim fading, gcer masi kebersamaan, dan akademik masih di atas 3,2. Goodluck kedepannya braderrrr\n");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
+    printf("\nini gw tinggal sini aja ya para aslab :)\n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
 }
