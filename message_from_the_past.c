fdf5a2b910769508168f61af4eba7eba0c946d67
aksioma admin <khalisazhrm@gmail.com>
2025-05-17 11:45:18 +0700

    Add message from lisa

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main() {
-    printf("\033[1;34m");
-    printf("Written by Tekkom UI 2024\n");
-    printf("\033[0m");
+    int main() {
+        printf("\033[1;34m");
+        printf("Written by Tekkom UI 2024\n");
+        printf("\033[0m");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
-}
+        printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
+        printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+        printf("From: lisa\n");
+        printf("hai all! for now, bentar lagi semester 2 selesai nih. thanks yaa buat semuanya! \nmakasih temen-temen, semester ini seru banget, bener-bener grateful bisa ngelewatin ini bareng kalian. terus makasih juga buat para aslab, makasih banget atas bantuan dan ilmunya selama praktikum. \ni hope pas time capsule ini dibuka, all our goals and wishes come true, dan semoga kita semua masih temenan <3\n");
+    }
