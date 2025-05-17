9bb892f633d214e74363555aa819fc244f8dbc17
Haitsams <harphypaw.09@gmail.com>
2025-05-17 21:36:26 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("\033[1;34m");
     printf("Written by Tekkom UI 2024\n");
-    printf("\033[0m");
 
-    printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
-    printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("first of all, makasih bang aslab-aslab Digilab yang udah selalu mau ngajarin dan bantu gua di matkul progdas ini.\n");
+    printf("belajar progdas itu fun tapi challenging juga bahkan banyak strugglenya apalagi modul-modul akhir,\n");
+    printf("but yeeah its worth it, i guees... soalnya gua masih bego bgt soal koding PS. maaf ya bang!")
 }
