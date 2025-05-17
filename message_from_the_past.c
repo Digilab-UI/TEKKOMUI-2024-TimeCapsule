8a76af8809b19e131a240e93a2d8bda21ea6ac4f
rifqifadill <rifqiitsnain7@gmail.com>
2025-05-17 09:09:26 +0700

    Add message from Rifqi

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,22 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+
+    printf("From: Anonim\n");
+    printf("Untuk aku di masa depan,\n");
+    printf("Kalau kamu sedang baca ini, berarti kamu sudah berjalan jauh dari tempat kamu berdiri sekarang.\n");
+    printf("Mungkin waktu itu kamu capek, bingung, bahkan ngerasa nggak mampu.\n");
+    printf("Tapi nyatanya kamu tetap bertahan dan itu sangat luar biasa.\n");
+    printf("Terima kasih sudah percaya sama diri sendiri, walau kadang rasanya pengin nyerah.\n");
+    printf("Jangan lupa istirahat, dan tolong jangan terlalu keras sama diri sendiri yaa.\n\n");
+
+    printf("Untuk para aslab yang sudah sabar dalam membimbing kami,\n");
+    printf("Terima kasih sudah rela luangin waktunya, tenaga, dan sabar buat bimbing praktikan yang serba bingung ini.\n");
+    printf("Kami tahu kadang kami ngerepotin nanya sesuatu yang udah dijelasin, kirim file telat, atau typo aneh di kode.\n");
+    printf("Tapi percayalah, bimbingan kalian sangat berarti buat kami. Semoga kalian juga terus dikelilingi kebaikan.\n");
+
+    printf("Salam hangat dari masa lalu :')\n");
+
+    return 0;
+
 }
