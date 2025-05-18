66ae4eb3376c19b1a545410c87829fd4a81b7b9d
thafinity <thalitasalmaa@gmail.com>
2025-05-18 20:35:26 +0700

    Add message from <thalita>

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,4 +7,6 @@ int main() {
 
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslabs 23: \n CH: christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
+    printf("Thank you abang abang digi udah sabar ngajarin kami satu semester ini,
+         semoga semua kegiatannya dilancarkan ya :> -thalita  \n");
 }
